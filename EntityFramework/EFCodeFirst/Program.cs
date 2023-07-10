77a02ade26b31b4e1e555f13d586f3caf62ab7d7
Selvasundar10 <127298248+Selvasundar10@users.noreply.github.com>
2023-07-10 23:42:56 +0530

    Add files via upload

---
+++ EntityFramework/EFCodeFirst/Program.cs
@@ -0,0 +1,15 @@
+﻿using EFCodeFirst;
+
+class Program
+{
+   public static void Main(string[] args)
+    {
+        CRUDOperation crud = new CRUDOperation();
+        /* crud.InsertRecordsInStudentEntity();
+         crud.InsertRecordsInCourseEntity();
+
+         crud.UpdateRecordsInStudentEntity();
+         crud.DeleteRecordsInStudentEntity();*/
+        crud.ReadDataFromStudentsEntity("Sundar");
+    }
+}
